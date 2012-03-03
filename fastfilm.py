e076aaf75768ce10eeb4102adfa013537dd4d93e
Jordi Burgos <jordimaister@gmail.com>
2012-03-03 18:07:08 +0100

    Project structure

---
+++ fastfilm.py
@@ -0,0 +1,8 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="jordi"
+__date__ ="$03-mar-2012 17:39:37$"
+
+if __name__ == "__main__":
+    print "Hello World"
