658b65c4b63f9c51d287f10e1772671dfb479d68
Jordi Burgos <jordimaister@gmail.com>
2012-03-03 18:22:46 +0100

    Project structure

--- js/scripts.js
+++ js/scripts.js
@@ -8,15 +8,10 @@ $(document).ready(function(){
    });
 
    var getPoster = function(){
-
         var film = $('#term').val();
-
          if(film == ''){
-
             $('#poster').html("<h2 class='loading'>Ha! We haven't forgotten to validate the form! Please enter something.</h2>");
-
          } else {
-
             $('#poster').html("<h2 class='loading'>Your poster is on its way!</h2>");
 
             $.getJSON("http://api.themoviedb.org/2.1/Movie.search/en/json/23afca60ebf72f8d88cdcae2c4f31866/" + film + "?callback=?", function(json) {
