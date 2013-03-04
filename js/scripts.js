157bd883a82e510923de5d2a519e5eba8f7c4e83
Jordi Burgos <jordiburgos@gmail.com>
2013-03-05 00:19:45 +0100

    Add autocomplete

--- js/scripts.js
+++ js/scripts.js
@@ -55,6 +55,33 @@ $(document).ready(function() {
         app.showPoster(1);
     });
 
+    $( "#term" ).autocomplete({
+        source: function( request, response ) {
+            $.ajax({
+                url: api_url + request.term + "?callback=?",
+                dataType: "jsonp",
+                success: function( data ) {
+                    response( $.map( data, function( item ) {
+                        return {
+                            label: item.original_name,
+                            value: item.original_name
+                        }
+                    }));
+                }
+            });
+        },
+        minLength: 2,
+        select: function( event, ui ) {
+            getPoster();
+        },
+        open: function() {
+            $( this ).removeClass( "ui-corner-all" ).addClass( "ui-corner-top" );
+        },
+        close: function() {
+            $( this ).removeClass( "ui-corner-top" ).addClass( "ui-corner-all" );
+        }
+    });
+
 });
 
 var current_data = {
